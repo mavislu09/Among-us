55fb719e92aea49f22dd44492d3b1c9bf903ca10
mavislu09 <dannacastillo09@gmail.com>
2020-09-21 22:19:15 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function imp() {
+    let impostor= true;
+
+    if (impostor == !true) {
+        alert("Eres un tripulante")
+  } else { 
+        alert("Eres un impostor")
+      } 
+}   
+
+function tri() {
+    let impostor= false;
+
+    if (impostor == !true) {
+        alert("Eres un tripulante")
+  } else { 
+        alert("Eres un impostor")
+      } 
+}   
+
+function dead() {
+      let muerto = true;
+  
+      if (muerto == !false) {
+          alert("Estás muerto")
+  } 
+}
